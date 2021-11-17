d1583f2d06351a6d22161fa4c807792475c7f47e
brunameinberg <brunalm2@al.insper.br>
2021-11-17 17:45:12 -0300

    jogo oficial (alterar esse documento em específico)

---
+++ Foxsupremacy.py
@@ -0,0 +1,126 @@
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+largura = 700
+altura = 600
+window = pygame.display.set_mode((largura, altura)) #tamanho da tela
+pygame.display.set_caption('Fox Supremacy') #nome do jogo
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ----- Inicia assets
+
+altura_jacare = 75
+largura_jacare = 80
+altura_fox = 75
+largura_fox = 80
+#C:/Users/lucas/Documents/Insper/1Semestre/Dessoft 2021.2/github/projetofinalpygame/ 
+fundo_de_tela = pygame.image.load('imagens/fundo.png').convert_alpha()
+fundo_de_tela = pygame.transform.scale(fundo_de_tela, (largura, altura))
+fundo_de_tela2 = pygame.image.load('imagens/fundo.png').convert_alpha()
+fundo_de_tela2 = pygame.transform.scale(fundo_de_tela, (largura, altura))
+fox_imagem = pygame.image.load('imagens/fox1.png').convert_alpha()
+fox_imagem = pygame.transform.scale(fox_imagem, (largura_fox, altura_fox))
+jacare_imagem = pygame.image.load('imagens/jacare.png').convert_alpha()
+jacare_imagem = pygame.transform.scale(jacare_imagem, (largura_jacare, altura_jacare))
+
+# variaveis globais
+x=0
+
+# ----- Inicia estruturas de dados
+# Definindo os novos tipo
+class Fox(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 300
+        self.rect.bottom = 550
+        self.speedx = 0
+        self.speedy = 0
+
+    def update(self):
+        # Atualização da posição da raposa
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+        # Mantem dentro da tela
+        if self.rect.right > largura:
+            self.rect.right = largura
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom==350:
+            self.speedy=5
+        if self.rect.bottom>550:
+            self.rect.bottom=550
+class Jacare(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 500
+        self.rect.bottom = 550
+        self.speedx = -1
+        self.speedy = 0
+
+    def update(self):
+        # Atualização da posição do jacaré
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+        # Mantem dentro da tela
+        if self.rect.right > largura:
+            self.rect.right = largura
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+clock = pygame.time.Clock()
+FPS = 60
+
+jogador = Fox(fox_imagem)
+inimigo = Jacare(jacare_imagem)
+
+
+# ===== Loop principal =====
+while game:
+
+    clock.tick(FPS)
+
+
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+        if event.type == pygame.KEYUP:
+            jogador.speedy=-5 #faz a raposa subir com 5 de velocidade
+        
+    # atualiza posição ( por enquanto zerada)7
+    jogador.update()
+    inimigo.update()
+
+    # ----- Gera saídas
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(fundo_de_tela, (x, 0))
+    window.blit(fundo_de_tela2, ((700+x), 0)) #anda o fundo da tela
+    window.blit(jogador.image, jogador.rect)
+    window.blit(inimigo.image, inimigo.rect)
+
+
+    
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+    x-=1
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
