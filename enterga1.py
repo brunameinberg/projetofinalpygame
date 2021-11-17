4a1de718a770635119a20cdacb9ef3711a2bf7ae
Lucas <lucasno@al.insper.edu.br>
2021-11-17 16:29:17 -0300

    att

--- enterga1.py
+++ enterga1.py
@@ -13,13 +13,16 @@ game = True
 
 # ----- Inicia assets
 
+altura_jacare = 75
+largura_jacare = 80
 altura_fox = 75
 largura_fox = 80
-fundo_de_tela = pygame.image.load('C:/Users/lucas/Documents/Insper/1Semestre/Dessoft 2021.2/github/projetofinalpygame/imagens/fundo.png').convert()
+fundo_de_tela = pygame.image.load('C:/Users/lucas/Documents/Insper/1Semestre/Dessoft 2021.2/github/projetofinalpygame/imagens/fundo.png').convert_alpha()
 fundo_de_tela = pygame.transform.scale(fundo_de_tela, (largura, altura))
-fox_imagem = pygame.image.load('C:/Users/lucas/Documents/Insper/1Semestre/Dessoft 2021.2/github/projetofinalpygame/imagens/fox1.png').convert()
+fox_imagem = pygame.image.load('C:/Users/lucas/Documents/Insper/1Semestre/Dessoft 2021.2/github/projetofinalpygame/imagens/fox1.png').convert_alpha()
 fox_imagem = pygame.transform.scale(fox_imagem, (largura_fox, altura_fox))
-
+jacare_imagem = pygame.image.load('C:/Users/lucas/Documents/Insper/1Semestre/Dessoft 2021.2/github/projetofinalpygame/imagens/jacare.png').convert_alpha()
+jacare_imagem = pygame.transform.scale(jacare_imagem, (largura_jacare, altura_jacare))
 
 
 # ----- Inicia estruturas de dados
@@ -37,7 +40,30 @@ class Fox(pygame.sprite.Sprite):
         self.speedy = 0
 
     def update(self):
-        # Atualização da posição da nave
+        # Atualização da posição da raposa
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+        # Mantem dentro da tela
+        if self.rect.right > largura:
+            self.rect.right = largura
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+class Jacare(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 500
+        self.rect.bottom = 550
+        self.speedx = 0
+        self.speedy = 0
+
+    def update(self):
+        # Atualização da posição do jacaré
         self.rect.x += self.speedx
         self.rect.y += self.speedy
 
@@ -48,10 +74,12 @@ class Fox(pygame.sprite.Sprite):
             self.rect.left = 0
 
 clock = pygame.time.Clock()
-FPS = 30
+FPS = 60
 
 jogador = Fox(fox_imagem)
+inimigo = Jacare(jacare_imagem)
 
+x=0
 # ===== Loop principal =====
 while game:
 
@@ -66,16 +94,22 @@ while game:
 
     # atualiza posição ( por enquanto zerada)7
     jogador.update()
+    inimigo.update()
 
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor branca
-    window.blit(fundo_de_tela, (0, 0))
+    window.blit(fundo_de_tela, (x, 0))
+    window.blit(fundo_de_tela, (700-x, 0))
     window.blit(jogador.image, jogador.rect)
+    window.blit(inimigo.image, inimigo.rect)
+
 
     
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
+
+    x-=10
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
